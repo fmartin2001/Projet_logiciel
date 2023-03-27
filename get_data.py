0c840fdc53cc4a744799f6b270cb862b6515ab42
lruyer <122295077+lruyer@users.noreply.github.com>
2023-03-27 11:32:21 +0200

    get_data commenté

--- get_data.py
+++ get_data.py
@@ -4,21 +4,21 @@ from PIL import Image
 import pandas as pd
 
 
-def load_dataset(path_to_data, nb_im):
+def charger_dataset(chemin_vers_data, nb_im):
     """
-     Args:
-        path_to_data (str): Path to the dataset
-        nb_im (str): Image number you want to load.
+    Arguments:
+        chemin_vers_data (str): Chemin vers le dataset
+        nb_im (str): Nombre d'images à charger
 
-    Returns:
-        np.array: 4D np array. Each row corresponds to an image. Each cell is a pixel number
+    Return:
+        img_pixel_list (np.array): 4D np array. Chaque ligne correspond à une image. Chaque case est un nombre de pixels.
     """
 
-    img_list = os.listdir(path_to_data)
+    img_list = os.listdir(chemin_vers_data)
     img_pixel_list = []
 
     for i in range(nb_im):
-        img = Image.open(f'{path_to_data}/{img_list[i]}')
+        img = Image.open(f'{chemin_vers_data}/{img_list[i]}')
         img = img.resize((60, 76))
         img = np.array(img)
         img_pixel_list.append(img)
@@ -29,25 +29,11 @@ def load_dataset(path_to_data, nb_im):
     return img_pixel_list
 
 
-# def img_id_attribute(attribute_list):
-#     df = pd.read_csv("./CelebA/Anno/list_attr_celeba.txt")
-#
-#     for attr in attribute_list:
-#         df = df.loc[df[attr] == 1]
-#
-#     return df
-
-
 if __name__ == "__main__":
-    nb_img_to_load = 4
-    img_pixel_list = load_dataset('./img_align_celeba/img_align_celeba',nb_img_to_load)
+    nb_img_a_charger = 4
+    img_pixel_list = charger_dataset('./img_align_celeba/img_align_celeba',nb_img_to_load)
 
-    path = f"./img_align_celeba/img_align_celeba/{nb_img_to_load}_img_pixel_list"
+    chemin = f"./img_align_celeba/img_align_celeba/{nb_img_a_charger}_img_pixel_list"
 
-    if not os.path.isfile(path):
-        np.save(path, img_pixel_list)
-
-    liste_attributs = ["Bangs", "Big_Lips", "Big_Nose"]
-    # mon_df = img_id_attribute(liste_attributs)
-
-    #df = pd.read_csv("./CelebA/Anno/list_attr_celeba.txt")
+    if not os.path.isfile(chemin):
+        np.save(chemin, img_pixel_list)
