ceda72171b36d4d2da555e925a70a0de94b8b75d
Clarisse Buton <107200281+bubiologie@users.noreply.github.com>
2023-03-27 17:22:08 +0200

    Update IG.py

--- IG.py
+++ IG.py
@@ -74,7 +74,7 @@ class FEN0(QWidget):
         self.label = QLabel(
             "Bienvenue dans un générateur de portrait robot ! \nNous vous prions de répondre le plus honnêtement possible afin de faire un protrait robot \nde votre agresseur des plus representatifs. \nLors du choix des visages, nous vous conseillons également de choisir le minimum de propositions. \nVeuillez appuyer sur demarrer quand vous serez prêt.")
         self.image_label = QLabel()
-        self.image_pixmap = QPixmap("logo.png")
+        self.image_pixmap = QPixmap("logo.jpg")
         self.image_label.setPixmap(self.image_pixmap.scaledToWidth(400))
         button = QPushButton("Démarrer", self)
         self.nextfen = FEN1()
